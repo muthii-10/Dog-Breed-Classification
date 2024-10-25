e70309083078df010a941eff1f9f0fec841dd812
Victor Muthii <145502833+muthii-10@users.noreply.github.com>
2024-10-25 11:24:34 +0300

    Update print_results.py

--- print_results.py
+++ print_results.py
@@ -1,11 +1,7 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# */AIPND-revision/intropyproject-classify-pet-images/print_results.py
-#                                                                             
-# PROGRAMMER: Victor Muthii
-# DATE CREATED: 22/07/2024
-# REVISED DATE: 
-# PURPOSE: Create a function print_results that prints the results statistics
+# -*- coding: utf-8 -*-                     
+
+# PURPOSE: Creates a function print_results that prints the results statistics
 #          from the results statistics dictionary (results_stats_dic). It 
 #          should also allow the user to be able to print out cases of misclassified
 #          dogs and cases of misclassified breeds of dog using the Results 
@@ -25,11 +21,7 @@
 #             False in the function call within main (defaults to False)
 #         This function does not output anything other than printing a summary
 #         of the final results.
-##
-# TODO 6: Define print_results function below, specifically replace the None
-#       below by the function definition of the print_results function. 
-#       Notice that this function doesn't to return anything because it  
-#       prints a summary of the results using results_dic and results_stats_dic
+
 # 
 def print_results(results_dic, results_stats_dic, model, 
                   print_incorrect_dogs = False, print_incorrect_breed = False):
